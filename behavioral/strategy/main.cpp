47430ff520cf24627e04054c9fe44dece37bfe5d
Vitaly Baldeev <Vitaly.Baldeev@infotecs.ru>
2019-08-30 12:08:15 +0300

    Added example for strategy pattern

--- behavioral/strategy/main.cpp
+++ behavioral/strategy/main.cpp
@@ -1,6 +1,16 @@
 #include "logger.h"
 
 int main() {
+
+     Logger logger(std::make_unique<StdStreamWriter>(), std::make_unique<AsIsTextBehavior>());
+     logger.write("Text as is to std out");
+
+     logger.setFormatTextBehavior(std::make_unique<UpperTextBehavior>());
+     logger.write("Upper text to std out");
+
+     logger.setWriteBehavior(std::make_unique<FileStreamWriter>());
+     logger.write("Upper text to file");
+
      return 0;
 }
 
